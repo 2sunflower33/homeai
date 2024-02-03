f25221c1e8e7dae08b6ddf159c5c48da61c0ce85
jingli <jingli@Roblox.com>
2024-02-03 14:08:59 -0800

    run with astra db

--- backend/app/engine/index.py
+++ backend/app/engine/index.py
@@ -3,23 +3,36 @@ import os
 from llama_index import (
     StorageContext,
     load_index_from_storage,
+    VectorStoreIndex, SimpleDirectoryReader, ServiceContext,
 )
-
+from llama_index.llms import HuggingFaceLLM
+from pathlib import Path
+from llama_index import download_loader
 from app.engine.constants import STORAGE_DIR
 from app.engine.context import create_service_context
+from llama_index.vector_stores import AstraDBVectorStore
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def get_chat_engine():
     service_context = create_service_context()
     # check if storage already exists
-    if not os.path.exists(STORAGE_DIR):
-        raise Exception(
-            "StorageContext is empty - call 'python app/engine/generate.py' to generate the storage first"
-        )
+    astra_db_store = AstraDBVectorStore(
+        token=os.getenv("ASTRA_DB_APPLICATION_TOKEN"),
+	api_endpoint=os.getenv("ASTRA_DB_API_ENDPOINT"),
+	collection_name="test",
+	embedding_dimension=1536,
+    )
+    PDFReader = download_loader("PDFReader")
+    loader = PDFReader()
+    documents = loader.load_data(file=Path('/Users/jingli/ragathon0203/homeai/backend/data/disclosure.pdf'))
     logger = logging.getLogger("uvicorn")
     # load the existing index
-    logger.info(f"Loading index from {STORAGE_DIR}...")
-    storage_context = StorageContext.from_defaults(persist_dir=STORAGE_DIR)
-    index = load_index_from_storage(storage_context, service_context=service_context)
-    logger.info(f"Finished loading index from {STORAGE_DIR}")
+    logger.info(f"Loading index from ...")
+    storage_context = StorageContext.from_defaults(vector_store=astra_db_store)
+    index = VectorStoreIndex.from_documents(
+        documents, storage_context=storage_context
+    )
+    logger.info(f"Finished loading index from ")
     return index.as_chat_engine(similarity_top_k=5, chat_mode="condense_plus_context")
